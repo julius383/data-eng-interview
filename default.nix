24d8f1b97a46cf47b7fb27130d6b364aca758837
Scott Milliken <scott@deltaex.com>
2022-01-31 18:06:51 -0800

    Remove dependency on 'import <nixpkgs>' to fix bug with nix 2.4+ and empty NIX_PATH env var.

--- default.nix
+++ default.nix
@@ -1,6 +1,11 @@
-let pkgs = (import <nixpkgs> {}); in
-let stdenv = pkgs.stdenv; in
-pkgs.mkShell rec {
+let
+    pkgs = (import (builtins.fetchGit {
+        url = "git@github.com:NixOS/nixpkgs.git";
+        ref = "nixos-21.11";
+        rev = "a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31";
+    }) { });
+    stdenv = pkgs.stdenv;
+in pkgs.mkShell rec {
     name = "interview";
     shellHook = ''
         source .bashrc
